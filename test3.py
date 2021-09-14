5dd9a5789037c79741f46691fd10b87482abac38
krishnaqa64 <krishnaqa64@gmail.com>
2021-09-14 16:04:52 -0400

    Examples

--- test3.py
+++ test3.py
@@ -1,3 +1,26 @@
+import numpy as np
 import pandas as pd
-
 df=pd.read_csv("data2/data2.csv",index_col=None)
+
+
+
+
+df['Conditions']=df['Weather'].apply(lambda x:"playable" if x=="Sunny" else "Cannot Play")
+print(df)
+
+def func1(x):
+    if x=="Sunny": return "Play"
+    elif x=="Cloudy": return "Doubtful"
+    else : return "Cannot play"
+
+df['Conditions']=df['Weather'].apply(func1)
+print(df)
+
+
+
+df.loc['Averages','Temperature']=df["Temperature"].mean()
+df.loc['Averages','Wind']=df["Wind"].mean()
+df.loc['Averages','Humidity']=df["Humidity"].mean()
+df.loc['Averages']=df.loc['Averages'].replace(np.NAN,"Not valid")
+
+print(df)
